9eaca3d06a6882a753637eb08ba3cdd44785c997
Alex Fox <alexfox@Alexs-MacBook-Pro.local>
2020-10-30 13:27:13 -0400

    added constants and fuel tests

--- converters.py
+++ converters.py
@@ -5,6 +5,11 @@ NPL = 4.44822 # newtons per pound-foot
 IPM = 1550 # square inches per square meter
 LPN = 0.224809 # pound-force per newton
 MPI = 0.00064516 # meters squared per inch squared
+MPK = 0.621372 # meters per kilometer
+LPG = 3.78541 # liters per gallon
+KPM = 1.60934 # kilometers per mile
+GPL = 0.264172 # gallons per liter
+
 
 def psi2kpa(psi):
     """
@@ -17,3 +22,15 @@ def kpa2psi(kpa):
     takes in a KPA value and converts it to PSI
     """
     return kpa * 1000 * LPN * MPI
+
+def mpg2LP100K(mpg):
+    """
+    takes in a mpg value and converts it to Liters per 100 Km
+    """
+    return mpg * MPK * LPG * 100
+
+def LP100K2mpg(LP100K):
+    """
+    takes in a liter per 100 km value and converts it to mpg
+    """
+    return LP100K * KPM * GPL / 100
--- converters_test.py
+++ converters_test.py
@@ -1,7 +1,7 @@
 """Unit tests for the units converter Library functions"""
 
 # inport the functions to be tested
-from converters import psi2kpa, kpa2psi
+from converters import psi2kpa, kpa2psi, mpg2LP100K, LP100K2mpg
 
 def describe_a_library_of_units_converters():
     """Tests suite for units conversion functions"""
@@ -11,8 +11,16 @@ def describe_a_library_of_units_converters():
 
     def can_convert_psi_to_kpa():
         assert psi2kpa(32) == 220.631712 # 32psi == 220.631712 KPA; average car tire pressure
-        assert psi2kpa(8.5) == 58.6052985 # 32psi == 58.6052985 KPA; average basketball pressure
+        assert psi2kpa(8.5) == 58.6052985 # 8.5psi == 58.6052985 KPA; average basketball pressure
  
     def can_convert_kpa_psi():
         assert kpa2psi(101.325) == 14.695952495133 # kpa to psi; average air pressure at sea level
         assert kpa2psi(220.631712) == 31.999932479367043 # kpa to psi; average car tire pressure
+
+    def can_convert_mpg_to_LP100K():
+        assert mpg2LP100K(1/40) == 5.8803694563 # gpm to LP100K; car that gets good gas mileage
+        assert mpg2LP100K(1/14.5) == 16.22170884496552 # gpm to LP100K; average diesel truck gas mileage
+
+    def can_convert_LP100K_to_mpg():
+        assert LP100K2mpg(5.8803694563) == 0.024999953625019847 # LP100k to gpm; car that gets good gas mileage
+        assert LP100K2mpg(36) == 0.15305132393280002 # LP100k to mpg; average semi-truck gas mileage
